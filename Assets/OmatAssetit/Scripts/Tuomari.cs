e491e453f008531c06fa70d5f893bc2e5570ee12
suklaasukkulayt <suklaasukkulayt@gmail.com>
2025-10-29 17:17:51 +0200

    uusin versio

--- Assets/OmatAssetit/Scripts/Tuomari.cs
+++ Assets/OmatAssetit/Scripts/Tuomari.cs
@@ -4,6 +4,9 @@ public class Tuomari : MonoBehaviour
 {
     private void OnTriggerEnter(Collider car)
     {
-        
+        CarIdentify id = car.GetComponent<CarIdentify>();
+
+        string winnerName = id.displayName;
+        Debug.Log($"WINNER: {winnerName}");
     }
 }
